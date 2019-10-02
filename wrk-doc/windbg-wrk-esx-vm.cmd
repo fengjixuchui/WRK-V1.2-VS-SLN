56d6cd78bb6af36e2a5fd0e524fa0c7dd777c9ce
song.min@live.com <song.min@live.com>
2019-10-02 13:32:28 +0800

    modify winddng start cmd script

--- wrk-doc/windbg-wrk-esx-vm.cmd
+++ wrk-doc/windbg-wrk-esx-vm.cmd
@@ -2,5 +2,5 @@ set volume_root=\\songmin-9010\e$
 set wrk=D:\git-repo\WRK-V1.2-VS-SLN
 set windbgargs=-k com:port=com1 -srcpath %wrk%\BASE\NTOS
 set _NT_SYMBOL_PATH=%wrk%\base\ntos\build\exe;%volume_root%\pdb;srv*%volume_root%\pdb*http://msdl.microsoft.com/download/symbols
-"C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\windbg.exe" %windbgargs%
+start "" "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\windbg.exe" %windbgargs%
 
